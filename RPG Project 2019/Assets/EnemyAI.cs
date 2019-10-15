18cd22c102a1c7d36036186ef1683929f31b7375
ChrisFoulkes <30705721+ChrisFoulkes@users.noreply.github.com>
2019-10-15 23:39:35 +0100

    Enemy Ai work in progress 0.1

---
+++ RPG Project 2019/Assets/EnemyAI.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+
+    public float updateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    public Path path;
+
+    public float speed = 300f;
+    public ForceMode2D fMode;
+    
+    public bool pathIsEnded = false;
+
+
+    public bool moving = true;
+    public bool attacking = true;
+
+    private int currentWaypoint = 0;
+
+    public float nextWaypointDistance = 3;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        GameObject targetgo = GameObject.FindGameObjectWithTag("Player");
+        target = targetgo.transform;
+        if (target == null) {
+            Debug.Log("Target not Found!");
+
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+
+    }
+
+
+    public void OnPathComplete(Path p) {
+        //Debug.Log("We got a  path. Did it error ? " + p.error);
+        if (!p.error) {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    IEnumerator UpdatePath() {
+        if (target == null) {
+            //TODO: Insert a path search here.
+            yield return false;
+        }
+
+
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+
+    void FixedUpdate()
+    {
+       // AiAttack();
+
+
+        if (Vector2.Distance(transform.position, target.position) <= 2f)
+        {
+            moving = false;
+
+        }
+        else {
+            moving = true;
+        }
+
+
+        if (moving)
+        {
+         
+            Move();
+        }
+    }
+
+
+    private void Move()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        if (path == null)
+        {
+            return;
+        }
+     
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+            {
+                return;
+            }
+
+            // Debug.Log("End of path reached.");
+
+            pathIsEnded = true;
+            return;
+        }
+
+        pathIsEnded = false;
+
+        //Direction to the next waypoint
+       
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+
+        dir *= speed * Time.fixedDeltaTime;
+        dir.z = 0;
+        rb.AddForce(dir);
+       
+        //transform.position = transform.position* speed * Time.fixedDeltaTime;
+        rb.AddForce(dir, fMode);
+
+
+        //if (dir.y > 0.1)
+        //{
+        //    dir = new Vector2(dir.x, 20f);
+
+        //}
+        //else
+        //{
+        //   rb.AddForce(dir, fMode);
+        //}
+
+        float dist = (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]));
+
+        if (dist < nextWaypointDistance)
+        {
+            Debug.Log("moving!");
+            currentWaypoint++;
+            return;
+        }
+    }
+
+
+    //basic Attack
+
+
+    public void AiAttack() {
+        if(CanAttack())
+        gameObject.GetComponent<Animator>().Play("AttackAnimation");
+
+    }
+
+    public bool CanAttack() {
+        return true;
+    }
+}
---
+++ RPG Project 2019/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ITakeDamage
+{
+
+    public EnemyData enemyData;
+
+    public List<Stat> enemyStats = new List<Stat>();
+
+    public float healthPoints;
+
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D colliderBox;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        colliderBox = GetComponent<BoxCollider2D>();
+        LoadEnemyData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadEnemyData() {
+        foreach (EnemyStatData statData in enemyData.statList) {
+            enemyStats.Add(new Stat(statData.startingvalue, statData.statName));
+        }
+
+        healthPoints = enemyData.healthPoints;
+
+        spriteRenderer.sprite = Resources.Load<Sprite>(enemyData.SpritePath);
+        colliderBox.size = spriteRenderer.size;
+    }
+
+    public void TakeDamage(float damage) {
+        healthPoints -= damage;
+    }
+
+
+
+
+}
